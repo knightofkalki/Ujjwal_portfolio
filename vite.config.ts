049dcfe41d8ed88f136c422eb7105c625a89a627
ujjwal aggarwal <ujjwalaggarwal@ujjwals-MacBook-Air.local>
2025-08-08 23:21:50 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Portfolio",
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: '/Ujjwal_portfolio/', // ← important: with slashes
 });
